b659aeae63000cc2a56bddaf762940db97c20d4a
dolphintwo <dtdinghui@126.com>
2016-12-20 17:13:58 +0800

    2016-12-20 17:13:52

---
+++ src/ch4/DemoObject.java
@@ -0,0 +1,11 @@
+package ch4;
+
+public class DemoObject {
+	public static void main(String[] args) {
+		String i = "aaaa";
+		String j = i.toString();
+		System.out.println(i.hashCode());
+		System.out.println(j);
+		System.out.println(j.hashCode());
+	}
+}
